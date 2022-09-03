f972e59775f9ed677bf4b9956b1c6fec2d35cd25
shakil <mdshakil2898@gmail.com>
2022-09-03 03:36:32 -0700

    spinner added

--- js/news.js
+++ js/news.js
@@ -3,6 +3,7 @@ const loadNewsCatagories = async() =>{
     const res = await fetch(url);
     const data = await res.json();
     return data;
+    
 }
 loadNewsCatagories();
 const displayCatagories = async() =>{
@@ -12,16 +13,19 @@ const displayCatagories = async() =>{
     
     arrayOfCatagories.forEach(element => {
         // console.log(element);
-
+        
         const {category_id, category_name} = element;
         const div = document.createElement('div');
+        div.classList.add('col');
         div.innerHTML =`
         <h6 onclick="getDetailNews('${category_id}','${category_name}')">${category_name}</h6>
         `;
         newsCatagories.appendChild(div);
         
+        
     });
     
+    // 
 }
 displayCatagories();
 
@@ -33,6 +37,7 @@ const getDetailNews= async(id,name) =>{
 }
 
 function displayDetailNews(data,name){
+  toggleSpinner(true);
     console.log(data,name)
     const newsDetail = data.data;
     // console.log(newsDetail.length);
@@ -55,23 +60,23 @@ function displayDetailNews(data,name){
         div.innerHTML = `
         <div class="row g-0">
         <div class="col-md-4">
-          <img src="${element.image_url}" class="img-fluid h-100 rounded-start" alt="...">
+          <img src="${element.image_url ? element.image_url : "no data available"}" class="img-fluid h-100 rounded-start" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">${element.title}</h5>
+            <h5 class="card-title">${element.title ? element.title : "no data available"}</h5>
             <p class="card-text">${element.details.length > 150 ? element.details.slice(0,150)+ "..." + "<br>" + "<br>" + element.details.slice(150,250) + "..."  : element.details}</p>
             <div>
             <div class="d-flex justify-content-between">
             <div class="d-flex gap-3">
-              <div><img class="author-img" src="${element.author.img}" alt=""></div>
+              <div><img class="author-img" src="${element.author.img ? element.author.img : "no image available right now"}" alt=""></div>
               <div>
-                <h6>${element.author.name}</h6>
+                <h6>${element.author.name ? element.author.name : "no data available"}</h6>
                 <p>${element.author.published_date ? element.author.published_date.slice(0,10) : "the information is not available"}</p>
               </div>
             </div>
             <div>
-              <p>${element.total_view}K</p>
+              <p>${element.total_view ? element.total_view + 'K' : "no data available"}</p>
             </div>
             <div><button onclick="newsDetails('${element._id}')" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#news-modal">
             View Details
@@ -84,6 +89,7 @@ function displayDetailNews(data,name){
         `;
         newsField.appendChild(div);
     })
+    toggleSpinner(false);
 }
 const newsDetails = async(id) => {
     const url = `https://openapi.programming-hero.com/api/news/${id}`;
@@ -112,6 +118,18 @@ function displaySingleNews (data){
         `;
         modalContainer.appendChild(div);
     })
+}
+
+// spinner function 
+const toggleSpinner = (spin) => {
+  const spinner = document.getElementById('spinner');
+        
+  if(spin){
+    spinner.classList.remove('d-none');
+  }
+  else{
+      spinner.classList.add('d-none');
+  }
 }
 
 // displayDetailNews();
